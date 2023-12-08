574852884aedf76258880b9b51d5fea4ca5b2ff3
ridhika123 <37682124+ridhika123@users.noreply.github.com>
2023-12-08 14:55:43 -0500

    Add files via upload

--- open_ai.py
+++ open_ai.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key="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",
+    api_key= "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" #"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",
 )
 MODEL = "gpt-3.5-turbo"
 
